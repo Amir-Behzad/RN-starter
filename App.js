df1d6fd0e20b97532f7baad9d389d383017c8285
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2020-05-15 18:05:17 +0300

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, Image, TextInput, View, ScrollView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ScrollView>
+        <Text>Well Hi</Text>
+        {/* style={styles.container */}
+      <View style={styles.container}>
+        <Text>Nasıl Gidiyor?</Text>
+        <Image source={{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}} style={{width: 200, height:200}} />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          width:500,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
